cfb4c59803823289e14c2b33208cc15ee252e722
Maciej Mróz <maciej.mroz@gmail.com>
2024-10-30 16:28:41 +0100

    Generic Circe Codec for UUID based opaque types (with helper From and Into typeclasses)

--- src/main/scala/barker/entities/OpaqueCompanion.scala
+++ src/main/scala/barker/entities/OpaqueCompanion.scala
@@ -4,6 +4,20 @@ import java.security.SecureRandom
 
 import java.util.UUID
 
+/** Inspired by Rust From/Into type classes, although not as nice to use.
+  */
+trait From[U, V]:
+  def from(value: U): V
+
+object From:
+  def apply[U, V](using ev: From[U, V]): From[U, V] = ev
+
+trait Into[U, V]:
+  def into(value: V): U
+
+object Into:
+  def apply[U, V](using ev: Into[U, V]): Into[U, V] = ev
+
 /** An experiment in providing common functionality to opaque types - looks like we can define a trait with both upper
   * and lower bounds of type parameter to the type being aliased by opaque type. This wouldn't make sense in Scala 2,
   * but in Scala 3 opaque types actually meet these bounds (as they _are_ the type). All that remains is deriving
@@ -22,3 +36,9 @@ trait UUIDCompanion[T >: UUID <: UUID]:
   def apply(id: UUID): T = id
   def random(): T = UUID.randomUUID()
   extension (id: T) def value: UUID = id
+
+  given From[UUID, T] with
+    def from(v: UUID): T = v
+
+  given Into[UUID, T] with
+    def into(v: T): UUID = v.value
--- src/main/scala/barker/schema/CirceSupport.scala
+++ src/main/scala/barker/schema/CirceSupport.scala
@@ -1,16 +1,18 @@
 package barker.schema
 
 import io.circe.{Codec, HCursor, Json}
-import barker.entities.UserId
+import barker.entities.{From, Into}
 import io.circe.Decoder.Result
 import java.util.UUID
 
-/** Circe codecs for basic domain types
-  *
-  * TODO: not sure if they really belong here, it would be massively easier to just do this inside entities package ...
+/** Circe codecs for basic domain types - might want to move it elsewhere in future, just did not want dependency on
+  * Circe in domain code, as JSON encoding/decoding is more of an infrastructure thing
   */
 object CirceSupport:
-  given Codec[UserId] = new Codec[UserId]:
-    override def apply(c: HCursor): Result[UserId] = c.as[String].map(s => UserId(UUID.fromString(s)))
+  private type FromUUID[T] = From[UUID, T]
+  private type IntoUUID[T] = Into[UUID, T]
 
-    override def apply(a: UserId): Json = Json.fromString(a.value.toString)
+  given uuidCodec[T: FromUUID: IntoUUID]: Codec[T] with
+    def apply(c: HCursor): Result[T] = c.as[String].map(s => From[UUID, T].from(UUID.fromString(s)))
+
+    def apply(a: T): Json = Json.fromString(Into[UUID, T].into(a).toString)
